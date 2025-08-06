ecf921f297038c751db9f1dda538c854fa2d7603
suresh adapa <sureshadapa11@gmail.com>
2025-08-06 23:50:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -90,5 +90,9 @@ def download_pdf():
 
     return send_file(pdf_path, as_attachment=True)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
